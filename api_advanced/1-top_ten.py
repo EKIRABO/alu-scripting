48f851cbf31788e516cee38aeedd92ef9523e83c
Esther Kirabo <e.kirabo@alustudent.com>
2025-10-25 13:09:44 +0200

    Top ten

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Print the titles of the first 10Hot Posts"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'User-Agent': 'Me:05'}
+    url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                    json_data.get('data')
+                    .get('children')[i]
+                    .get('data')
+                    .get('title')
+                )
+    else:
+        print(None)
