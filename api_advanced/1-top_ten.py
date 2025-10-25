da69cbda4f2e0c0d0fd0ffc5e06d6be7dd8148a7
EKIRABO <e.kirabo@alustudent.com>
2025-10-25 13:20:26 +0200

    Refactor top_ten function to improve post retrieval

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+""" Print the titles of the first 10 Hot Posts."
 import requests
 
 
 def top_ten(subreddit):
+    """ Prints titles of first 10 hot posts """
     headers = {'User-Agent': 'Me:05'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
-
+    url = "https://reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
+        posts = json_data.get('data').get('children')
+        for post in posts:
+            print(post.get('data').get('title'))
     else:
-        print(OK)
+        print(None)
