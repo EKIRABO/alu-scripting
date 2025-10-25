56bebca67e4f67d24411e53626b6e18bd9201a80
EKIRABO <e.kirabo@alustudent.com>
2025-10-25 12:50:21 +0200

    Fix typo in response status check

--- api_advanced/ 0-subs.py
+++ api_advanced/ 0-subs.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 import requests
 def number_of_subscribers(subreddit):
     url="https://www.reddit.com/r/{}/about.json"
 
         headers={'User-Agent':'My User Agent 5.0'}
         response=requests.get(url,headers=headers)
-        if response.status_code== 200:
+        If response.status_code== 200:
             return response.json()
         return 0
